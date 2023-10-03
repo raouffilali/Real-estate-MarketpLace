5d6352a657c8773bdb5a60f9ba300b9ed121eb1f
Abderraouf FILALI <filaliabderaouf54@gmail.com>
2023-10-03 21:30:55 +0100

    Component layout

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { images } from "../constants"
+const Header = () => {
+  return (
+    <header>
+      <img src={images.LOGO} alt="" />
+    </header>
+  );
+}
+
+export default Header
---
+++ client/src/constants/images.js
@@ -0,0 +1,8 @@
+import Logo from '../assets/AzhomeLOGO.PNG';
+import Logo2 from '../assets/AzhomeLOGO.PNG';
+
+
+export const images = {
+    LOGO: Logo,
+    LOGO2: Logo2,
+};
